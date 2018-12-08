78390c18ea925cdca74f17a3d1eb5423fb53d40d
stewart leach <stew.leach@gmail.com>
2018-12-08 18:09:34 +1100

    integrating pug-bootstrap to render templates

--- exchange-renderer/src/app.ts
+++ exchange-renderer/src/app.ts
@@ -1,13 +1,19 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import ExpressRouter from './express.router';
+import bodyParser from 'body-parser';
+import path from 'path';
 
 dotenv.config();
 
 const port = 8000;
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 app.set('views', './src/views');
+app.use(express.static(path.join(__dirname, '../src')));
+
 
 const expressRoutes = new ExpressRouter(app);
 expressRoutes.init();
