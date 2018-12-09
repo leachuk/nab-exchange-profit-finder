2c66320df31f0aac1f250cfb3f39550749ebe69f
stewart leach <stew.leach@gmail.com>
2018-12-09 16:52:15 +1100

    refactoring to simplyfy and pass data from cmd line

--- exchange-renderer/src/app.ts
+++ exchange-renderer/src/app.ts
@@ -1,6 +1,5 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import ExpressRouter from './express.router';
 import bodyParser from 'body-parser';
 import path from 'path';
 
@@ -14,13 +13,14 @@ app.set('view engine', 'pug');
 app.set('views', './src/views');
 app.use(express.static(path.join(__dirname, '../src')));
 
-
-const expressRoutes = new ExpressRouter(app);
-expressRoutes.init();
-
 //get value passed from commandline option
 const argv = require('yargs').argv;
 console.log(JSON.parse(argv.exchangejson));
+
+app.get('/', function (req,res,next) {
+    //var data = { title: 'Hey', message: 'Hello there!', listinfo: ["foo1", "bar1", "zoo1"] };
+    res.render('index', JSON.parse(argv.exchangejson));
+})
 
 app.listen(port, () => {
   console.log(`Express server app listening on port ${port}!`);
