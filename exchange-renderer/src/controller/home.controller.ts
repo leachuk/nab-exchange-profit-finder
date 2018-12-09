1b45859f0ad63795888f98dec39b4013b03f9f37
stewart leach <stew.leach@gmail.com>
2018-12-09 13:49:16 +1100

    integrating json array sample into pug template

--- exchange-renderer/src/controller/home.controller.ts
+++ exchange-renderer/src/controller/home.controller.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from 'express'
 export default class HomeController {
 
   public static getDefault(req: Request, res: Response, next: NextFunction) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+    var data = { title: 'Hey', message: 'Hello there!', listinfo: ["foo", "bar", "zoo"] };
+    res.render('index', data);
   }
 }
